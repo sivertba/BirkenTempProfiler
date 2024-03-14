93f98f0187d788451da40698d83d43429801171a
sivertba <sivert.bakken@gmail.com>
2024-03-14 12:28:41 +0100

    Use list comprehensions

--- makeProfiles.py
+++ makeProfiles.py
@@ -1,29 +1,32 @@
 import os
-durations = [ # HH:MM
-    (3,30),
-    (4,0),
-    (4,30),
-    (5,0),
-    (5,30),
-    (6,0),    
-]
 
-StartTimes = [ # HH:MM, Assuming 2024-03-16
-    ("07","30"),
-    ("08","00"),
-    ("08","30"),
-    ("09","00"),
-    ("09","30"),
-]
+durations = [(hh,mm) for hh in range(3,8) for mm in [45, 30, 15, 0] ]
+
+# Assuming 2024-03-16
+StartTimes = [ (str(hh), str(mm)) for hh in range(7,10) for mm in [45, 30, 15, 0] ]
 
 for dur in durations:
     for st in StartTimes:
 
+        hh = str(st[0])
+        mm = str(st[1])
+
+        if len(hh) == 1:
+            hh = "0" + hh
+        if len(mm) == 1:
+            mm = "0" + mm
+
         cmd = "python3 birkentempprofiler.py -r 'rennet' "
-        cmd += "-s '2024-03-16T" + st[0] + ":" + st[1] + ":00' "
+        cmd += "-s '2024-03-16T" + hh + ":" + mm + ":00' "
         cmd += "-t " + str(dur[0]) + " -m " + str(dur[1])
         print(cmd)
         os.system(cmd)
-        newName = f"temperatureProfile_{st[0]}_{st[1]}_T_{dur[0]}_{dur[1]}.html"
+
+        folderPath = "profiles"
+        if not os.path.exists(folderPath):
+            os.makedirs(folderPath)
+
+        newName = f"temperatureProfile_{hh}_{mm}_T_{dur[0]}_{dur[1]}.html"
+        newName = os.path.join(folderPath, newName)
         os.rename("temperatureProfile.html", newName)
 
